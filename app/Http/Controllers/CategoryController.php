3feb38fb7f271b4e4856e2fa8cfc4850fd8e9dca
msyrh <hidayatullohmuhammadsyarif@gmail.com>
2020-02-26 13:14:09 +0700

    sample tambah role admin

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -13,7 +13,10 @@ use Excel;
 class CategoryController extends Controller
 {
     
-    
+    public function __construct()
+    {
+        $this->middleware('role:admin');
+    }
     /**
      * Display a listing of the resource.
      *
